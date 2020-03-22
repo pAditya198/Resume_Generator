31add52af2126dd150f8067b48f23a1dd33090c6
Aditya Prakash <p.aditya.198@gmail.com>
2020-03-23 01:32:06 +0530

    basic design header

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import { saveAs } from "file-saver";
 import "./App.css";
 
 class App extends Component {
-state = {
-      name: "",
-      phone: 0,
-      githubUrl: 0,
-    };
-  
-handleChange = ({ target: { value, name }}) => this.setState({ [name]: value })
+  state = {
+    name: "",
+    gmail: "",
+    twitter: "",
+    linkdin: "",
+    github: ""
+  };
 
-createAndDownloadPDF = () => {
-  axios.post('/create-pdf', this.state)
-  .then(() => axios.get('fetch-pdf', { responseType: 'blob' }))
-  .then((res) => {
-    const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
+  handleChange = ({ target: { value, name } }) =>
+    this.setState({ [name]: value });
 
-    saveAs(pdfBlob, 'newPdf.pdf');
-  })
-}
+  createAndDownloadPDF = () => {
+    axios
+      .post("/create-pdf", this.state)
+      .then(() => axios.get("fetch-pdf", { responseType: "blob" }))
+      .then(res => {
+        const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+
+        saveAs(pdfBlob, "newPdf.pdf");
+      });
+  };
   render() {
     return (
       <div className="App">
@@ -30,16 +34,29 @@ createAndDownloadPDF = () => {
           name="name"
           onChange={this.handleChange}
         />
+
         <input
-          type="number"
-          placeholder="Phone"
-          name="phone"
+          type="url"
+          placeholder="Gmail"
+          name="gmail"
           onChange={this.handleChange}
         />
         <input
           type="url"
-          placeholder="Github Link"
-          name="githubUrl"
+          placeholder="Twitter"
+          name="twitter"
+          onChange={this.handleChange}
+        />
+        <input
+          type="url"
+          placeholder="Linkdin"
+          name="linkdin"
+          onChange={this.handleChange}
+        />
+        <input
+          type="url"
+          placeholder="Github"
+          name="github"
           onChange={this.handleChange}
         />
         <button onClick={this.createAndDownloadPDF}>Download PDF</button>
--- server/documents/index.js
+++ server/documents/index.js
@@ -1,53 +1,54 @@
-module.exports = ({ name, phone, githubUrl }) => {
+module.exports = ({ name, gmail,twitter,linkdin, github }) => {
   const today = new Date();
   const arr = ["kirito", "aditya"];
   const loop = arr.map(ele => {
     return `<p>${ele}</p>`;
   });
-
-  return `
-   <!doctype html>
-   <html>
-      <head>
-         <meta charset="utf-8">
-         <title>PDF Result Template</title>
-         <style>
-         .container {
-            max-width: max-content;
-            margin: auto;
-            padding: 30px;
-            border: 1px solid #eee;
-            box-shadow: 0 0 10px rgba(0, 0, 0, .15);
-            font-size: 14px;
-            line-height: 24px;
-            font-family: 'Helvetica Neue', 'Helvetica';
-            color: #555;
-        }
-        
-        .container header table td {
-            padding: 5px;
-            vertical-align: top;
-        }
-        
-
-        .container table tr.top table td {
-            padding-bottom: 20px;
-        }
-         </style>
-      </head>
-      <body>
-      <div class="container">
-      <header>
-      <table>
-         <tr>
-            <td><strong> Name</strong> : ${name} </td>
-            <td><strong>Contact</strong> : ${phone} </td>
-            <td><strong>Github Url</strong> : ${githubUrl} </td>
-         </tr>
-      </table>
-      </header>
-   </div>
-      </body>
-   </html>
+  const path = "http://github.com/iiitv.png";
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Resume</title>
+     <script src="https://use.fontawesome.com/72777e07a4.js"></script>
+     <style>
+     .container {
+      max-width: 800px;
+      margin: auto;
+      padding: 25px;
+      padding-right: 10px;
+      border: 1px solid #eee;
+      box-shadow: 0 0 10px rgba(0, 0, 0, .15);
+      line-height: 24px;
+      font-family: 'Helvetica Neue', 'Helvetica';
+  }
+  
+  .header .name{
+      text-align: center;
+  }
+  
+  .header .imp-links tr td{
+  font-size: 12px;
+  font-weight: bold;
+  padding: 7.5px;
+  }
+     </style>
+  </head>
+  <body class="container">
+     <header class="header">
+        <h1 class="name">${name}</h1>
+        <table class="imp-links">
+           <tr>
+              <td><i class="fa fa-at"></i> ${gmail}</td>
+              <td><i class="fa fa-twitter"></i> ${twitter} </td>
+              <td><i class="fa fa-linkedin"></i> ${linkdin} </td>
+              <td><i class="fa fa-github"></i> ${github} </td>
+           </tr>
+        </table>
+     </header>
+     
+  </body>
+  </html>
    `;
 };
